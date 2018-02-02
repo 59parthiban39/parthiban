68f2dbed649b7db140c6d9fe78782a19ac36aa70
59parthiban39 <35764937+59parthiban39@users.noreply.github.com>
2018-02-02 13:27:04 +0530

    parthi1.c
    parthi1.c

---
+++ parthi1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the number:\n");
+	scanf("%d",&a);
+	if(a>0)
+{
+	printf("positive");
+}
+	else if(a<0)
+{	printf("negative");
+}
+	if(a=0)
+{	printf("zero");
+}
+	return 0;
+}
