e5b144e9e145fa2fefdfc67304f25931e1e6cd84
59parthiban39 <35764937+59parthiban39@users.noreply.github.com>
2018-02-05 16:11:44 +0530

    Update PARTHI33.C

--- PARTHI33.C
+++ PARTHI33.C
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include<conio.h>
 int main()
 {
     char c;
@@ -8,8 +9,9 @@ int main()
     isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
     if (isLowercaseVowel || isUppercaseVowel)
-        printf("%c is a vowel.", c);
+        printf("%c vowel.", c);
     else
-        printf("%c is a consonant.", c);
+        printf("%c consonant.", c);
+    getch();
     return 0;
 }
